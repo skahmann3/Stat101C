38d41b90b9d05a8472a9175c1afe77dd15284ba2
skahmann3 <skahmann3@gmail.com>
2017-05-31 14:33:19 -0700

    With dates

--- XGBoost.R
+++ XGBoost.R
@@ -1,5 +1,19 @@
 ## XG Boost
 
+## Adding date/time variables
+load("C:/Users/Sydney/Desktop/training.RData")
+
+library(lubridate)
+training$`Incident Creation Time (GMT)` <- 
+  hms(training$`Incident Creation Time (GMT)`)
+
+training$hour <- hour(training$`Incident Creation Time (GMT)`)
+training$min <- minute(training$`Incident Creation Time (GMT)`)
+training$second <- second(training$`Incident Creation Time (GMT)`)
+
+# training[,c(3:8, 11)] <- as.data.frame(apply(training[,c(3:8,11)], 2, as.factor))
+
+
 # ## Practicing with 25% of the original training data
 # practice.train <- sample(c(1:2317430), 2317430*.25, replace=FALSE)
 # training2 <- training[practice.train,]
@@ -44,16 +58,26 @@ train2 <- as.data.frame(apply(train2, 2, as.numeric))
 test2 <- as.data.frame(apply(test2, 2, as.numeric))
 
 
-xg = xgboost(data=data.matrix(train2[,1:3]), label=data.matrix(train2[,4]), nrounds = 500, early_stopping_rounds = 20, eta=.1)
-preds.xgb=predict(xg, newdata=data.matrix(test2[,1:3]))
+xg = xgboost(data=data.matrix(train2[,c(1:3,5:7)]), label=data.matrix(train2[,4]), 
+             nrounds = 100, early_stopping_rounds = 20, eta=.1)
+preds.xgb=predict(xg, newdata=data.matrix(test2[,c(1:3,5:7)]))
 mse.xgb=mean((preds.xgb-test2[,4])^2)
 mse.xgb
 
 
+load("C:/Users/Sydney/Desktop/testing.RData")
+
+testing$`Incident Creation Time (GMT)` <- 
+  hms(testing$`Incident Creation Time (GMT)`)
+
+testing$hour <- hour(testing$`Incident Creation Time (GMT)`)
+testing$min <- minute(testing$`Incident Creation Time (GMT)`)
+testing$second <- second(testing$`Incident Creation Time (GMT)`)
+
 testing2 <- testing[,-c(1,2,6,7,8,9)]
 testing2 <- as.data.frame(apply(testing2, 2, as.numeric))
 
-preds.xgb=predict(xg, newdata=data.matrix(testing2[,1:3]))
+preds.xgb=predict(xg, newdata=data.matrix(testing2[,c(1:6)]))
 
 result <- cbind(testing$row.id, preds.xgb)
 colnames(result) <- c("row.id", "prediction")
