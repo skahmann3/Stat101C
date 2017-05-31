d69beb1a61a0a212a83024d9002f3358e3383680
skahmann3 <skahmann3@gmail.com>
2017-05-31 12:43:06 -0700

    XGBoost code, only using the three original variables that were numeric

--- XGBoost.R
+++ XGBoost.R
@@ -1,14 +1,13 @@
 ## XG Boost
 
-### Practicing with 25% of the original training data
+# ## Practicing with 25% of the original training data
+# practice.train <- sample(c(1:2317430), 2317430*.25, replace=FALSE)
+# training2 <- training[practice.train,]
 
-practice.train <- sample(c(1:2317430), 2317430*.25, replace=FALSE)
-training2 <- training[practice.train,]
+i.train <- sample(c(1:nrow(training)), nrow(training)*.75, replace=FALSE)
 
-i.train <- sample(c(1:579357), 579357*.75, replace=FALSE)
-
-test2 <- training2[-i.train,]
-train2 <- training2[i.train,]
+test2 <- training[-i.train,]
+train2 <- training[i.train,]
 
 
 ### XG Boost doesn't use categorical variables, only numeric
@@ -18,6 +17,7 @@ train2 <- training2[i.train,]
 # removing the row.id, incident.id, incident creation time variable
 test2 <- test2[,-c(1,2,6,7,8,9)]
 train2 <- train2[,-c(1,2,6,7,8,9)]
+
 
 ### Now all variables are factors or numeric variables
 # train2$`Dispatch Status` <- factor(train2$`Dispatch Status`)
@@ -35,41 +35,27 @@ train2 <- train2[,-c(1,2,6,7,8,9)]
 #                       "Dispatch_Sequence", "Dispatch_Status", 
 #                       "Unit_Type", "PPE_Level", "elapsed_time")
 
-#using one hot encoding 
-
-
-
-labels <- train2$elapsed_time 
-ts_label <- test2$elapsed_time
-
-# new_tr <- matrix(train2)
-# new_ts <- matrix(test2)
-
-# new_tr <- model.matrix(~.+0,data = train2[,-8]) 
-# new_ts <- model.matrix(~.+0,data = test2[,-8])
-
-str(train2)
-train2 <- as.data.frame(train2)
-
-#preparing matrix 
-new_tr[] <- lapply(train2, as.numeric)
-xgb.DMatrix(data=data.matrix(new_tr))
-
-dtrain <- xgb.DMatrix(data = new_tr,label = labels) 
-dtest <- xgb.DMatrix(data = new_ts,label=ts_label)
-
-
-library(Matrix)
-sparse_matrix <- sparse.model.matrix(elapsed_time~.-1, data = train2)
-
-head(sparse_matrix)
-
-response = train2$elapsed_time
-
 
 ### Building the model
 
 library(xgboost)
 
-bst <- xgboost(data = sparse_matrix, label = response, max.depth = 4,
-               eta = 1, nthread = 2, nround = 10, objective = "reg:linear")
+train2 <- as.data.frame(apply(train2, 2, as.numeric))
+test2 <- as.data.frame(apply(test2, 2, as.numeric))
+
+
+xg = xgboost(data=data.matrix(train2[,1:3]), label=data.matrix(train2[,4]), nrounds = 500, early_stopping_rounds = 20, eta=.1)
+preds.xgb=predict(xg, newdata=data.matrix(test2[,1:3]))
+mse.xgb=mean((preds.xgb-test2[,4])^2)
+mse.xgb
+
+
+testing2 <- testing[,-c(1,2,6,7,8,9)]
+testing2 <- as.data.frame(apply(testing2, 2, as.numeric))
+
+preds.xgb=predict(xg, newdata=data.matrix(testing2[,1:3]))
+
+result <- cbind(testing$row.id, preds.xgb)
+colnames(result) <- c("row.id", "prediction")
+write.csv(result, file = "C:/Users/Sydney/Desktop/xgboost.csv")
+
