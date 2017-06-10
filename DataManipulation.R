6ffa1edac484265e223a81d3e52b3a42b6b6d74a
skahmann3 <skahmann3@gmail.com>
2017-06-10 16:25:02 -0700

    additional xgboost code

--- DataManipulation.R
+++ DataManipulation.R
@@ -3,15 +3,19 @@
 ### Maybe should make district a factor?
 
 ### Training
-load("C:/Users/Sydney/Desktop/training.RData")
+# load("C:/Users/Sydney/Desktop/training.RData")
+
+library(readr)
+train_added <- read_csv("C:/Users/Sydney/Google Drive/101_Kaggle/train.NA.added.csv")
+
 
 library(lubridate)
-training$`Incident Creation Time (GMT)` <- 
-  hms(training$`Incident Creation Time (GMT)`)
+train_added$Incident.Creation.Time..GMT. <- 
+  hms(train_added$Incident.Creation.Time..GMT.)
 
-training$hour <- hour(training$`Incident Creation Time (GMT)`)
-training$min <- minute(training$`Incident Creation Time (GMT)`)
-training$second <- second(training$`Incident Creation Time (GMT)`)
+train_added$hour <- hour(train_added$Incident.Creation.Time..GMT.)
+train_added$min <- minute(train_added$Incident.Creation.Time..GMT.)
+train_added$second <- second(train_added$Incident.Creation.Time..GMT.)
 
 # Model did way worse with these variables (like 18 million worse for the MSE!)
 # so, maybe the correlation between the variables is a problem?
@@ -19,37 +23,46 @@ training$second <- second(training$`Incident Creation Time (GMT)`)
 # library(dplyr) 
 # training <- training %>% group_by(incident.ID) %>% mutate(NumUnits = n(), MaxDispatch = max(`Dispatch Sequence`))
 
-# removing the row.id, incident.id, time
-training2 <- training[,-c(1,2, 9)]
+# removing some columns
+training2 <- train_added[,-c(1,7,8,9,11,12,13,14)]
+training2 <- as.data.frame(training2)
+row.names(training2) <- train_added$row.id
 
 ### One hot encoding -- converting the categoricals to numerical variables
 
 ###  taking out spaces in variable names 
-colnames(training2) <- c("year", "First_in_District",
+# colnames(training2) <- c("year", "First_in_District",
                       "Dispatch_Sequence", "Dispatch_Status",
                       "Unit_Type", "PPE_Level", 
                       "elapsed_time", "hour", "min", "second")
 
-training2[,c("Dispatch_Status", "Unit_Type", "PPE_Level")] <- as.data.frame(apply(training2[,c("Dispatch_Status", "Unit_Type", "PPE_Level")], 2, as.factor))
+# training2[,c("Dispatch_Status", "Unit_Type", "PPE_Level")] <- as.data.frame(apply(training2[,c("Dispatch_Status", "Unit_Type", "PPE_Level")], 2, as.factor))
 
 
 
 
 ### Testing
-load("C:/Users/Sydney/Desktop/testing.RData")
+# load("C:/Users/Sydney/Desktop/testing.RData")
 
-testing$`Incident Creation Time (GMT)` <- 
-  hms(testing$`Incident Creation Time (GMT)`)
+library(readr)
+testing_added <- read_csv("C:/Users/Sydney/Google Drive/101_Kaggle/testing.added.csv", 
+   col_types = cols(Incident.Creation.Time..GMT. = col_character(), 
+   X1 = col_skip()))
 
-testing$hour <- hour(testing$`Incident Creation Time (GMT)`)
-testing$min <- minute(testing$`Incident Creation Time (GMT)`)
-testing$second <- second(testing$`Incident Creation Time (GMT)`)
+testing_added$Incident.Creation.Time..GMT. <- 
+  hms(testing_added$Incident.Creation.Time..GMT.)
+
+testing_added$hour <- hour(testing_added$Incident.Creation.Time..GMT.)
+testing_added$min <- minute(testing_added$Incident.Creation.Time..GMT.)
+testing_added$second <- second(testing_added$Incident.Creation.Time..GMT.)
 
 # Model did worse with these variables
 # testing <- testing %>% group_by(incident.ID) %>% mutate(NumUnits = n(), MaxDispatch = max(`Dispatch Sequence`))
 
-# removing the row.id, incident.id, time
-testing2 <- testing[,-c(1,2, 9)]
+# removing some columns
+testing2 <- testing_added[,-c(6,7,9,10,11,12)]
+testing2 <- as.data.frame(testing2)
+row.names(testing2) <- testing_added$row.id
 
 colnames(testing2) <- c("year", "First_in_District",
                          "Dispatch_Sequence", "Dispatch_Status",
--- XGboost_NumOnly.R
+++ XGboost_NumOnly.R
@@ -25,43 +25,60 @@ library(xgboost)
 # https://cran.r-project.org/web/packages/xgboost/vignettes/discoverYourData.html
 
 # selecting numeric variables only
-train2 <- train2[,c(1:3,7:10)]
-test2 <- test2[,c(1:3,7:10)]
+# train2 <- train2[,c(1:3,7:10)]
+# test2 <- test2[,c(1:3,7:10)]
 
 
 ## Everything must be "numeric", "integer" doesn't work
 train2 <- as.data.frame(apply(train2, 2, as.numeric))
 test2 <- as.data.frame(apply(test2, 2, as.numeric))
 
-
 ################## CODE FOR NUMERIC VARIABLES ONLY
 ## Making the model, switch out the column numbers when more variables are added
 ## Data = should point to all variables but the elapsed time variable
 ## Label = should point to the elapsed time variable
-xg = xgboost(data=data.matrix(train2[,c(1:3,5:7)]), label=data.matrix(train2[,4]),
-             nrounds = 500, early_stopping_rounds = 20, eta=.1)
-preds.xgb=predict(xg, newdata=data.matrix(test2[,c(1:3,5:7)]))
-mse.xgb=mean((preds.xgb-test2[,4])^2)
+xg = xgboost(data=data.matrix(train2[,c(1:5,7:9)]), label=data.matrix(train2[,6]),
+             nrounds = 100, early_stopping_rounds = 20, eta=.1)
+preds.xgb=predict(xg, newdata=data.matrix(test2[,c(1:5,7:9)]))
+mse.xgb=mean((preds.xgb-test2[,6])^2)
 mse.xgb ## training MSE
 
 
 ### Making predictions for submission using all data to train
 
-train2 <- training2[,c(1:3,7:10)]
-train2 <- as.data.frame(apply(train2, 2, as.numeric))
+# train2 <- training2[,c(1:3,7:10)]
+# train2 <- as.data.frame(apply(train2, 2, as.numeric))
 
-xg = xgboost(data=data.matrix(train2[,c(1:3,5:7)]), label=data.matrix(train2[,4]),
-             nrounds = 500, early_stopping_rounds = 20, eta=.1)
+xg = xgboost(data=data.matrix(training2[,c(1:6,8:10)]), label=data.matrix(training2[,7]),
+             nrounds = 100, early_stopping_rounds = 20, eta=.1)
+# XG boost. Variables: Year, incident count, avg.travel, avg.turnout, dispatch seq, hour, min, seconds to predict elapsed_time from dataset added. nrounds = 500.
+
+## no incident count var
+xg2 = xgboost(data=data.matrix(training2[,c(1,3:5,7:9)]), label=data.matrix(training2[,6]),
+             nrounds = 200, early_stopping_rounds = 20, eta=.1)
+# XG boost. Variables: Year, avg.travel, avg.turnout, dispatch seq, hour, min, seconds to predict elapsed_time from dataset na_added. nrounds = 500.
+
+
+
+
+
 
 test2 <- testing2[,c(1:3,7:9)]
 test2 <- as.data.frame(apply(test2, 2, as.numeric))
 
-preds.xgb=predict(xg, newdata=data.matrix(test2))
+preds.xgb=predict(xg, newdata=data.matrix(testing2))
+
+## no incident count var
+preds.xgb2=predict(xg2, newdata=data.matrix(testing2[,-2]))
 
 
 ## Create the csv for submission
 ## I then delete out the extra row number variable and save the columns in
 ########## number format in Excel
-result <- cbind(testing$row.id, preds.xgb)
+result <- cbind(row.names(testing2), preds.xgb)
 colnames(result) <- c("row.id", "prediction")
 write.csv(result, file = "C:/Users/Sydney/Desktop/xgboost.csv")
+
+result2 <- cbind(row.names(testing2), preds.xgb2)
+colnames(result2) <- c("row.id", "prediction")
+write.csv(result2, file = "C:/Users/Sydney/Desktop/xgboost2.csv")
